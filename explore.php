64e2213270387f4b04d75fdb1066794213a16735
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2023-01-25 18:44:02 -0300

    Estrutura da página de exploar plantas.

---
+++ explore.php
@@ -0,0 +1,27 @@
+<?php
+?>
+
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Plantê - Explorar</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+
+	<body>
+		<?php include "header.php"; ?>
+
+		<div class="sidebar">
+			<h3>Ornamentais</h3>
+			<input type="button" value="Cactus">
+			<input type="button" value="Rosas do deserto">
+			<input type="button" value="Palmeiras">
+		</div>
+
+		<div id="container-results">
+			<?php //TODO ?>
+		</div>
+
+		<?php include "footer.php"; ?>
+	</body>
+</html>
