b1aedd9c0a5283c74899144955ca6f9d918f5380
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2023-01-25 17:52:22 -0300

    Rodapé.

---
+++ footer.php
@@ -0,0 +1,12 @@
+<?php
+?>
+
+<footer>
+	<div class="logo-area">
+		<img src="" alt="Logo">
+		<strong>Plantê</strong>
+	</div>
+	<div class="contact-info">
+		<p>gabcchaves@zohomail.com</p>
+	</div>
+</footer>
