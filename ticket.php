078ef1054aa1c6bd33903fb3e6908e1757fd1677
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2023-01-25 19:59:57 -0300

    Estrutura da página de geração de boleto.

---
+++ ticket.php
@@ -0,0 +1,19 @@
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Boleto</title>
+		<link rel="stylesheet" href="css/ticket.css">
+	</head>
+
+	<body>
+		<?php include "header.php"; ?>
+
+		<h3>Seu boleto</h3>
+		<hr>
+		<p>Seu boleto de pagamento foi gerado. Vencimento em até 1 dia útil. A
+data de entrega é computada a partir do processamento do boleto.</p>
+		<button id="btn-print-ticket">Imprimir Boleto</button>
+
+		<?php include "footer.php"; ?>
+	</body>
+</html>
