c782a66a3ae7eaccd085b240ddb1c48a77864ac7
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2023-01-25 17:46:38 -0300

    Estrutura do cabeçalho reutilizável.

---
+++ header.php
@@ -0,0 +1,26 @@
+<?php
+?>
+
+<header>
+	<div class="logo-area">
+		<img src="" alt="Logo">
+		<strong>Plantê</strong>
+	</div>
+
+	<div class="options">
+		<a href="">Explorar</a>
+		<a href="">Carrinho</a>
+	</div>
+
+	<div class="search-area">
+		<form action="" method="POST">
+			<input type="text" placeholder="Pesquisar">
+			<input type="submit" value="Pesquisar">
+		</form>
+	</div>
+
+	<div class="user-options">
+		<a href="">Entrar</a>
+		<a href="">Registrar</a>
+	</div>
+</header>
