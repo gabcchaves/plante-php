ae4ca14afdd1b8fe0f3908981bcf6dd0e5a2c021
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2023-01-25 20:00:32 -0300

    Estrutura da página de configurações.

---
+++ settings.php
@@ -0,0 +1,25 @@
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Configurações</title>
+		<link rel="stylesheet" href="css/settings.css">
+	</head>
+
+	<body>
+		<?php include "header.php"; ?>
+
+		<div class="sidebar">
+			<h3>Configurações</h3>
+			<input type="button" value="Nome">
+			<input type="button" value="Endereço de e-mail">
+			<input type="button" value="Senha">
+			<input type="button" value="Telefone">
+		</div>
+
+		<div id="container-setting">
+			<?php //TODO ?>
+		</div>
+
+		<?php include "footer.php"; ?>
+	</body>
+</html>
