ff57d4406a6604f1f727dc245e756548ad06d566
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2023-01-25 19:46:43 -0300

    Estrutura da página de carrinho.

---
+++ cart.php
@@ -0,0 +1,18 @@
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Carrinho</title>
+		<link rel="stylesheet" href="css/cart.css">
+	</head>
+
+	<body>
+		<?php include "header.php"; ?>
+
+		<h3>Carrinho</h3>
+		<button id="btn-pay">Pagar</button>
+		<hr>
+		<?php // TODO ?>
+
+		<?php include "footer.php"; ?>
+	</body>
+</html>
