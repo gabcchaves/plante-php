80c1b1a97855c4044edbe80eb3c774c1a661b8f1
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2023-01-25 18:09:01 -0300

    Página inicial é index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+?>
+
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Plantê</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+
+	<body id="page-main">
+		<?php include "header.php"; ?>
+
+		<section id="introduction">
+			<h3>Bem-vindo(a) ao Plantê!</h3>
+			<p>Um sistema de comércio de plantas ornamentais.</p>
+		</section>
+
+		<section id="random-plants">
+			<?php //TODO ?>
+		</section>
+
+		<?php include "footer.php"; ?>
+	</body>
+</html>
