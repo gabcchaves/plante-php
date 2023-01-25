2c0ce05e8f7aeb7da0c8f01c5ce2d0fbaea41d56
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2023-01-25 20:00:15 -0300

    Estrutura da página de geração de chave PIX.

---
+++ pixkey.php
@@ -0,0 +1,18 @@
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Chave PIX</title>
+		<link rel="stylesheet" href="css/pixkey.css">
+	</head>
+
+	<body>
+		<?php include "header.php"; ?>
+
+		<h3>Chave PIX</h3>
+		<hr>
+		<p>Esta chave é válida por 30 minutos.</p>
+		<p id="pixkey"></p>
+
+		<?php include "footer.php"; ?>
+	</body>
+</html>
