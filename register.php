0c7576ceccc030770da946b0315b8ca70a55a4b5
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2023-01-25 18:36:14 -0300

    Estrutura da página de cadastro.

---
+++ register.php
@@ -0,0 +1,25 @@
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Cadastrar</title>
+		<link rel="stylesheet" href="css/register.css">
+	</head>
+
+	<body>
+		<?php include "header.php"; ?>
+
+		<h1>Cadastrar-se</h1>
+		<hr>
+		<form action="modules/register.php" method="POST">
+			<input name="name" type="text" placeholder="Nome completo">
+			<input name="email" type="email" placeholder="Endereço de e-mail">
+			<input name="password" type="password" placeholder="Senha">
+			<input type="password" placeholder="Confirmar senha">
+			<input name="phone" type="tel" placeholder="Telefone">
+			<input type="submit" value="Confirmar">
+		</form>
+		<small><a href="">Já tem conta?</a></small>
+
+		<?php include "footer.php"; ?>
+	</body>
+</html>
