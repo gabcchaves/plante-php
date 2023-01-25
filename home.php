399336136776d78782d36b7a25fdf19c503edd63
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2023-01-25 17:52:14 -0300

    Página inicial.

---
+++ home.php
@@ -0,0 +1,11 @@
+<?php
+?>
+
+<section id="introduction">
+	<h3>Bem-vindo(a) ao Plantê!</h3>
+	<p>Um sistema de comércio de plantas ornamentais.</p>
+</section>
+
+<section id="random-plants">
+	<?php //TODO ?>
+</section>
