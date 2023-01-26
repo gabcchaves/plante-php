bc4e93c87e7e79e02d144dfba17d596e200e997d
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2023-01-26 15:07:27 -0300

    Modificação do cabeçalho para ser como o da versão em RUST.

--- header.php
+++ header.php
@@ -2,28 +2,38 @@
 ?>
 
 <header id="main-header">
-	<div class="logo-area">
-		<img src="img/bx-leaf.svg" alt="Logo">
-		<strong>Plantê</strong>
-	</div>
-
-	<div class="options">
-		<a href="" class="css-link-light">Explorar</a>
-		<a href="" class="css-link-light">Carrinho</a>
-	</div>
-
-	<div class="search-area">
-		<form action="" method="POST">
-			<input type="text" placeholder="Pesquisar">
-			<button id="btn-search" type="submit">
-				<img src="img/bx-search.svg" alt="Pesquisar">
+	<header id="main-header">
+		<div class="dropdown-menu">
+			<button id="btn-menu">
+				<img class="icon-light" src="img/bx-dots-horizontal-rounded.svg" alt="Menu"/>
 			</button>
-		</form>
-	</div>
+			<div class="dropdown-content">
+				<a href="login.php">Entrar</a>
+				<a href="register.php">Cadastrar-se</a>
+			</div>
+		</div>
+		<div class="logo-area">
+			<img src="img/bx-leaf.svg" alt="Logo.">
+			<strong>Plantê</strong>
+		</div>
 
-	<div class="user-options">
-		<button id="menu" class="css-icon-btn"><img src="img/bx-dots-horizontal-rounded.svg"></button>
-		<a href="" class="css-link-light">Entrar</a>
-		<a href="" class="css-link-light">Registrar</a>
-	</div>
+		<div class="main-options">
+			<a href="explore.php">Explorar</a>
+			<a href="cart.php">Carrinho</a>
+		</div>
+
+		<div class="search-area">
+			<form id="search-form">
+				<input name="search-pattern" id="search-field" type="text" placeholder="Pesquisar"/>
+				<button type="submit">
+					<img src="img/bx-search.svg" alt="Pesquisar"/>
+				</button>
+			</form>
+		</div>
+
+		<div class="user-options">
+			<a href="login.php">Entrar</a>
+			<a href="register.php">Cadastrar-se</a>
+		</div>
+	</header>
 </header>
