ca55d1280bc6b7a7c5a1bec1e12dfa2917b23c22
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2023-01-26 16:07:57 -0300

    Aprimoramento da estrutura do cabeçalho.

--- header.php
+++ header.php
@@ -2,38 +2,38 @@
 ?>
 
 <header id="main-header">
-	<header id="main-header">
-		<div class="dropdown-menu">
-			<button id="btn-menu">
-				<img class="icon-light" src="img/bx-dots-horizontal-rounded.svg" alt="Menu"/>
+	<div class="dropdown-menu">
+		<button id="btn-menu">
+			<img class="css-icon-light" src="img/bx-dots-horizontal-rounded.svg" alt="Menu"/>
+		</button>
+		<div class="dropdown-content">
+			<a class="css-link-light" href="login.php">Entrar</a>
+			<a class="css-link-light" href="register.php">Cadastrar-se</a>
+		</div>
+	</div>
+
+	<div class="logo-area">
+		<img src="img/bx-leaf.svg" alt="Logo.">
+		<strong>Plantê</strong>
+		<a href="index.php"></a>
+	</div>
+
+	<div class="main-options">
+		<a class="css-link-light" href="explore.php">Explorar</a>
+		<a class="css-link-light" href="cart.php">Carrinho</a>
+	</div>
+
+	<div class="search-area">
+		<form id="search-form">
+			<input name="search-pattern" id="search-field" type="text" placeholder="Pesquisar"/>
+			<button type="submit">
+				<img class="css-icon-light" src="img/bx-search.svg" alt="Pesquisar"/>
 			</button>
-			<div class="dropdown-content">
-				<a href="login.php">Entrar</a>
-				<a href="register.php">Cadastrar-se</a>
-			</div>
-		</div>
-		<div class="logo-area">
-			<img src="img/bx-leaf.svg" alt="Logo.">
-			<strong>Plantê</strong>
-		</div>
+		</form>
+	</div>
 
-		<div class="main-options">
-			<a href="explore.php">Explorar</a>
-			<a href="cart.php">Carrinho</a>
-		</div>
-
-		<div class="search-area">
-			<form id="search-form">
-				<input name="search-pattern" id="search-field" type="text" placeholder="Pesquisar"/>
-				<button type="submit">
-					<img src="img/bx-search.svg" alt="Pesquisar"/>
-				</button>
-			</form>
-		</div>
-
-		<div class="user-options">
-			<a href="login.php">Entrar</a>
-			<a href="register.php">Cadastrar-se</a>
-		</div>
-	</header>
+	<div class="user-options">
+		<a class="css-link-light" href="login.php">Entrar</a>
+		<a class="css-link-light" href="register.php">Cadastrar-se</a>
+	</div>
 </header>
