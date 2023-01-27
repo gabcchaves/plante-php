119e9a1a16c1063d593015bf5a0f1cfa138af09c
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2023-01-27 08:38:19 -0300

    Campo de enderço de imagem de planta.

--- doc/plantedb.sql
+++ doc/plantedb.sql
@@ -15,14 +15,15 @@ CREATE TABLE IF NOT EXISTS plants (
 	name VARCHAR(50) NOT NULL,
 	description TEXT NOT NULL,
 	price DOUBLE NOT NULL,
-	quantity INT NOT NULL
+	quantity INT NOT NULL,
+	imagesource TEXT NOT NULL
 );
 
-INSERT INTO plants (name, description, price, quantity)
-VALUES ('Jasmim', 'Planta ornamental e medicinal.', 2.00, 10);
-INSERT INTO plants (name, description, price, quantity)
-VALUES ('Hibisco', 'Planta ornamental e medicinal.', 2.00, 10);
-INSERT INTO plants (name, description, price, quantity)
-VALUES ('Rosa do deserto', 'Planta ornamental.', 4.00, 10);
-INSERT INTO plants (name, description, price, quantity)
-VALUES ('Azaléia', 'Planta ornamental.', 2.00, 10);
+INSERT INTO plants (name, description, price, quantity, imagesource)
+VALUES ('Jasmim', 'Planta ornamental e medicinal.', 2.00, 10, 'https://wallpapercave.com/wp/wp9660102.jpg');
+INSERT INTO plants (name, description, price, quantity, imagesource)
+VALUES ('Hibisco', 'Planta ornamental e medicinal.', 2.00, 10, 'https://wallpapercave.com/wp/wp5530093.jpg');
+INSERT INTO plants (name, description, price, quantity, imagesource)
+VALUES ('Rosa do deserto', 'Planta ornamental.', 4.00, 10, 'https://wallpapercave.com/wp/wp5706237.jpg');
+INSERT INTO plants (name, description, price, quantity, imagesource)
+VALUES ('Azaléia', 'Planta ornamental.', 2.00, 10, 'https://wallpapercave.com/wp/wp6507134.jpg');
